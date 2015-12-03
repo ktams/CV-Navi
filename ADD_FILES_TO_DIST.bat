b4cf6059945f3d46daa76f64125d1a879703793f
LotharDG <lrnews@arcor.de>
2015-12-03 16:41:41 +0100

    Fix fuer Windows basiertes Build-Skript

--- ADD_FILES_TO_DIST.bat
+++ ADD_FILES_TO_DIST.bat
@@ -3,5 +3,5 @@ copy Zusatz-Dateien\CV-Navi.sh       dist\CV-Navi.sh
 
 copy Zusatz_Bibs\rxtxSerial.dll         dist\rxtxSerial.dll
 copy Zusatz_Bibs\rxtxSerial64.dll       dist\rxtxSerial64.dll
-copy Zusatz_Bibs\librxtxSerial.jnilib   dist\rxtxSerial.dll
-copy Zusatz_Bibs\librxtxSerial64.jnilib dist\rxtxSerial64.dll
+copy Zusatz_Bibs\librxtxSerial.jnilib   dist\librxtxSerial.jnilib
+copy Zusatz_Bibs\librxtxSerial64.jnilib dist\librxtxSerial64.jnilib
