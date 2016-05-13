188c416c24d252ce79e559947cbe4f3e907a28cb
LotharDG <lrnews@arcor.de>
2016-05-13 16:21:51 +0200

    Compile-Skript: Version von Netbeans automatisch ermitteln

--- DO_COMPILE.sh
+++ DO_COMPILE.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
+## try to find actual netbeans version on the fly
+ANT_VERSION=`find ${HOME}/.netbeans/ -name build.properties | sed -e "s#${HOME}/.netbeans/##" -e "s#/build.properties##"|sort -n | tail -1`
+
 if [ -z ${1} ] ; then
-  ant -f . -Duser.properties.file=${HOME}/.netbeans/8.0/build.properties -Dnb.internal.action.name=build jar
+  ant -f . -Duser.properties.file=${HOME}/.netbeans/${ANT_VERSION}/build.properties -Dnb.internal.action.name=build jar
 else
   if [ ${1} == all ] ; then
-    ant -f . -Duser.properties.file=${HOME}/.netbeans/8.0/build.properties -Dnb.internal.action.name=rebuild clean jar
+    ant -f . -Duser.properties.file=${HOME}/.netbeans/${ANT_VERSION}/build.properties -Dnb.internal.action.name=rebuild clean jar
     [ -f ADD_FILES_TO_DIST.sh ] && ./ADD_FILES_TO_DIST.sh
   fi
 fi
