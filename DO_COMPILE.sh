1938523ac0bc3c37ca75a71aae09a32420714c57
LotharDG <lrnews@arcor.de>
2016-06-11 18:17:15 +0200

    Compile-Skript: Korrektur Netbeans-Version ermitteln

--- DO_COMPILE.sh
+++ DO_COMPILE.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## try to find actual netbeans version on the fly
-ANT_VERSION=`find ${HOME}/.netbeans/ -name build.properties | sed -e "s#${HOME}/.netbeans/##" -e "s#/build.properties##"|sort -n | tail -1`
+ANT_VERSION=`find ${HOME}/.netbeans/ -name build.properties | sed -e "s#${HOME}/.netbeans/##" -e "s#/build.properties##"|sort -t \. -k 1,1n -k 2,2n -k 3,3n|tail -1`
 
 if [ -z ${1} ] ; then
   ant -f . -Duser.properties.file=${HOME}/.netbeans/${ANT_VERSION}/build.properties -Dnb.internal.action.name=build jar
