8db1ed57ae246cf6a63d48b4a7090ba02188c70c
LotharDG <lrnews@arcor.de>
2017-01-05 12:06:03 +0100

    Release 2.4 (20170105a)

--- src/my/KlarText/KlarTextUI.java
+++ src/my/KlarText/KlarTextUI.java
@@ -2511,7 +2511,7 @@ public class KlarTextUI extends javax.swing.JFrame {
         String osInfo = "( "+osName+"["+osArch+"] , java "+dataModel+"bit )";
         jLabelOS.setText(osInfo);
 
-        String gsBuild ="beta 20170104b"; // use keyword "beta" or "release"
+        String gsBuild ="release 20170105a"; // use keyword "beta" or "release"
         System.out.println("Build: "+gsBuild);
         if( debugLevel > 0 || gsBuild.contains("beta") ) {
             jLabelBuild.setText(gsBuild);
