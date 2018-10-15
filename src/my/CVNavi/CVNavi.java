a76a0b14e598a5413358736ee178ea3da04e1e0a
LotharDG <lrnews@arcor.de>
2018-10-15 18:02:49 +0200

    Vorbereitung für beta 3.1

--- src/my/CVNavi/CVNavi.java
+++ src/my/CVNavi/CVNavi.java
@@ -194,8 +194,8 @@ enum decoderList {
 
 public class CVNavi extends javax.swing.JFrame {
     /** Creates new form CVNavi */
-    public int     gsVersionMayor = 3;  // Version 3.0
-    public int     gsVersionMinor = 0;  // Version 3.0
+    public int     gsVersionMayor = 3;  // Version 3.1
+    public int     gsVersionMinor = 1;  // Version 3.1
     public JFrame  frameInstanceDEVICE = null;
     public JFrame  frameInstanceOPTIONS = null;
     public JFrame  frameInstanceINFO = null;
@@ -2601,7 +2601,7 @@ public class CVNavi extends javax.swing.JFrame {
         String osInfo = "("+osName+"["+osArch+"] , java "+javaversion+"["+dataModel+"bit])";
         jLabelOS.setText(osInfo);
 
-        String gsBuild ="release 20181015a"; // use keyword "beta" or "release"
+        String gsBuild ="beta 20181015b"; // use keyword "beta" or "release"
         System.out.println("Build: "+gsBuild);
         if( debugLevel > 0 || gsBuild.contains("beta") ) {
             jLabelBuild.setText(gsBuild);
