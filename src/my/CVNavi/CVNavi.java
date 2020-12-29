231ae7fe5af37fe8fbe51e08343c2b58d3c712be
LotharDG <lrnews@arcor.de>
2020-12-29 14:43:18 +0100

    Beta 3.3 20201229a  Bibliotheken-Updates   - AbsoluteLayout auf 122   - beansbinding 1.2.1 (kleinere Version)   - jna auf 5.6.0   - purejavacomm auf 1.0.3.2

--- src/my/CVNavi/CVNavi.java
+++ src/my/CVNavi/CVNavi.java
@@ -198,8 +198,8 @@ enum decoderList {
 
 public class CVNavi extends javax.swing.JFrame {
     /** Creates new form CVNavi */
-    public int     gsVersionMajor = 3;  // Version 3.2
-    public int     gsVersionMinor = 2;  // Version 3.2
+    public int     gsVersionMajor = 3;  // Version 3.3
+    public int     gsVersionMinor = 3;  // Version 3.3
     public JFrame  frameInstanceDEVICE = null;
     public JFrame  frameInstanceOPTIONS = null;
     public JFrame  frameInstanceINFO = null;
@@ -2651,7 +2651,7 @@ public class CVNavi extends javax.swing.JFrame {
         String osInfo = "("+osName+"["+osArch+"] , java "+javaversion+"["+dataModel+"bit])";
         jLabelOS.setText(osInfo);
 
-        String gsBuild ="release 20201228a"; // use keyword "beta" or "release"
+        String gsBuild ="beta 20201229a"; // use keyword "beta" or "release"
         System.out.println("Build: "+gsBuild);
         if( debugLevel > 0 || gsBuild.contains("beta") ) {
             jLabelBuild.setText(gsBuild);
